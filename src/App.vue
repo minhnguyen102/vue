c7a7d50b5565402a527dcf286276705381918bc3
Minh Nguyen <minhkhac1002@gmail.com>
2026-03-24 10:55:47 +0700

    syntax

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+  const text = 'Hello'
+  const rawHtml = '<span style="color: blue">This should be red.</span>'
+  const button_id = 'button'
+  const button_class = 'buttonClass'
+  const disableButton = false
+  const styleButton = {
+    style: {
+      color: 'red'
+    }
+  }
+
+  const sum = (a, b) => a + b
+</script>
+
+<template>
+  <h1>{{ text + ' world' }}</h1>
+  <div v-html="rawHtml"></div>
+  <button :id="button_id" :class="button_class" :disabled="disableButton" v-bind="styleButton">{{disableButton? 'Disable' : 'Click here'}}</button>
+  <div>
+    {{ sum(1, 2) }}
+  </div>
+</template>
+
+<style scoped>
+</style>
